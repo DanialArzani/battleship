2197aac9fdece8597244b2cabfb7971cd11d079d
Fazel montazery <ryan.montazery@protonmail.com>
2025-03-16 13:56:59 +0330

    Add playground class
    An abstraction for the matrix behind the game field

---
+++ playground.py
@@ -0,0 +1,15 @@
+from raylib import *
+
+class Playground:
+    def __init__(self, width, height):
+        this.width = width
+        this.height = height
+        this.matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+        create()
+
+    # initialize
+    def create():
+        pass
+
+    def draw():
+        pass
