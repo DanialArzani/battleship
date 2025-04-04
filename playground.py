55e6e6e3cc1b60acd39ae263166b1b77473761a2
DanialArzani <afm7303@gmail.com>
2025-04-04 17:46:41 +0330

    add create function in playground

--- playground.py
+++ playground.py
@@ -1,14 +1,19 @@
 from raylib import *
+from state import State
 
 class Playground:
     def __init__(self, width, height):
-        this.width = width
-        this.height = height
-        this.matrix = [[0 for _ in range(height)] for _ in range(width)]
+        self.width = width
+        self.height = height
+        self.matrix = [[0 for _ in range(width)] for _ in range(height)]
         create()
 
     # initialize
     def create():
+        for row in range(height):
+            for section in range(width):
+                water_block = self.matrix[row][section]
+                water_block = State(water_block) 
         pass
 
     def draw():
--- state.py
+++ state.py
@@ -10,7 +10,3 @@ class State(Enum):
     MISSED_SHOT = 6
 
 
-#State = Enum('State',[('WATER',0),('SMALL_SHIP',1),('MEDIUM_SHIP',2),('BIG_SHIP',3),('TREASURE',4)])
-
-s = State
-print(s(2).name)
